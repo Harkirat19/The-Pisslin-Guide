596d56dd7372e8f669664354b7224ea95a08ee2c
Enikő Valkó <eaaenva@students.eaaa.dk>
2024-07-01 14:29:59 +0200

    Tabs Styling

--- constants/ThemeVariables.jsx
+++ constants/ThemeVariables.jsx
@@ -2,7 +2,7 @@ export const Colors = {
   yellow: "#f8e5a4", // That healthy pee color
   background: "#f9cc31", // That unhealthy yellow pee
   gray: "#dfe2e3", // Gray stuff
-  darkgray: "#dfe2e3", // Background for tab
+  darkgray: "#757778", // Background for tab
   text: "#2a2b2c", // Black
   red: "#eb4c29",
   pink: "#f9c5c1",
