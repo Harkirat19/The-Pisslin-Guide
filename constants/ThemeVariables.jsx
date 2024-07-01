510930f437de6ad506a62aededc91f069465735b
Frederik Graakjær <frede.flip@gmail.com>
2024-07-01 14:12:16 +0200

    theme

--- constants/ThemeVariables.jsx
+++ constants/ThemeVariables.jsx
@@ -1,19 +1,9 @@
-const backgroundYellow = "#f9cc31";
-const primary = "#f4f005";
-const secondary = "#f8e5a4";
-const black = "#2a2b2c";
-const dark_grey = "#757778";
-const light_grey = "#dfe2e3";
-const red = "#eb4c29";
-const pink = "#f9c5c1";
-
-export {
-  primary,
-  black,
-  dark_grey,
-  light_grey,
-  backgroundYellow,
-  secondary,
-  red,
-  pink,
+export const Colors = {
+  yellow: "#f8e5a4", // That healthy pee color
+  background: "#f9cc31", // That unhealthy yellow pee
+  gray: "#dfe2e3", // Gray stuff
+  darkgray: "#dfe2e3", // Background for tab
+  text: "#2a2b2c", // Black
+  red: "#eb4c29",
+  pink: "#f9c5c1",
 };
