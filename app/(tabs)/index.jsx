ab079bc4806a9492b9819a2f1dfadf7a28cfbacd
Enikő Valkó <eaaenva@students.eaaa.dk>
2024-07-01 11:19:06 +0200

    Remove Post component for now

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,5 +1,4 @@
 import { StyleSheet } from 'react-native';
-
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 
@@ -8,7 +7,6 @@ export default function TabOneScreen() {
     <View style={styles.container}>
       <Text style={styles.title}>Home</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <Post/>
     </View>
   );
 }
